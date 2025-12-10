f37471e2868e3c5f638440e41ed78d7e02111680
Naitik Jain <150283488+naitiikjainn@users.noreply.github.com>
2025-12-10 19:03:08 +0530

    changed links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,8 +27,8 @@ function Projects() {
               isBlog={false}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/souyajit4419/Chatify"
+              demoLink="https://chatify-49.eb.app/"
             />
           </Col>
 
@@ -38,8 +38,8 @@ function Projects() {
               isBlog={false}
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/soumyait4419/Editor.io"
+              demoLink="https://editor.soumya-it.tech/"              
             />
           </Col>
 
